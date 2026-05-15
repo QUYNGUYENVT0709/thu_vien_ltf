7af6d11b4e2abaa3e22aed535b2a92567aabd264
QUYNGUYENVT0709 <163317408+QUYNGUYENVT0709@users.noreply.github.com>
2026-05-15 22:48:17 +0700

    a

--- tao_danh_sach.rb
+++ tao_danh_sach.rb
@@ -1,15 +1,14 @@
 require 'json'
 require 'digest'
-puts "Dang quet thu vien de tao ltf_manifest.json..."
+
 manifest = {}
 Dir.glob("**/*.skp").each do |file_path|
   file_hash = Digest::SHA256.file(file_path).hexdigest
   chuoi_duong_dan = file_path.gsub("\\", "/")
   manifest[chuoi_duong_dan] = file_hash
-  puts "Da quet: #{chuoi_duong_dan}"
 end
+
 File.open("ltf_manifest.json", "w") do |f|
   f.write(JSON.pretty_generate(manifest))
 end
-puts "\nThanh cong! Da quet #{manifest.keys.length} file."
-puts "Hay day file ltf_manifest.json nay cung voi cac file .skp len GitHub."
+puts "Da tu dong tao file ltf_manifest.json thanh cong tren GitHub Server!"
